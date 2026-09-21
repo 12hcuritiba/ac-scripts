6640bf63d4abfee74619a6ccadfd44b58eafefbf
12h Curitiba <12hcuritiba@gmail.com>
2026-09-21 19:28:27 -0300

    Add files via upload

---
+++ forcecockpitonline.lua
@@ -0,0 +1,36 @@
+-- force_cockpit_online.lua
+-- Script ONLINE do CSP. Forca a camera de cockpit como diretiva da sessao.
+--
+-- Guardas ativas (campos de ac.StateSim, via global "sim"):
+--   sim.isPaused        -> menu do ESC aberto
+--   sim.isReplayActive  -> replay em andamento
+-- Sem guarda para teleporte, reset e outros apps: nesses casos forca mesmo.
+
+local cfg = ac.configValues({
+  CHECK_INTERVAL  = 0.05,
+  ADMIN_STEAM_IDS = ''
+})
+
+local exempt = false
+do
+  local me = ac.getUserSteamID()
+  if me and cfg.ADMIN_STEAM_IDS ~= '' then
+    for id in string.gmatch(cfg.ADMIN_STEAM_IDS, '[^;]+') do
+      if id:match('^%s*(.-)%s*$') == me then exempt = true end
+    end
+  end
+end
+
+local timer = 0
+
+function script.update(dt)
+  if exempt then return end
+  if sim.isPaused or sim.isReplayActive then return end
+
+  timer = timer + dt
+  if timer < cfg.CHECK_INTERVAL then return end
+  timer = 0
+
+  ac.setCurrentCamera(ac.CameraMode.Car)
+  ac.setCurrentCamera(cfg.CAR_CAMERA_INDEX)
+end
